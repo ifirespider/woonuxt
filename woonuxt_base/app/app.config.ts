5db66e2e0184d91bbeef61d4f622149e8696b98a
ifirespider <ifirespider@gmail.com>
2026-09-11 15:23:41 +0800

    1

--- woonuxt_base/app/app.config.ts
+++ woonuxt_base/app/app.config.ts
@@ -4,10 +4,10 @@
  * Below are the default values.
  */
 export default defineAppConfig({
-  siteName: 'WooNuxt',
-  shortDescription: 'This is an example of a WooNuxt store. It provides a modern, fast, and SEO friendly ecommerce store built with Nuxt and WooCommerce.',
-  description: `WooNuxt is unmatched when it comes to performance and scalability. Reap the benefits of having a online store that out performs all of your competitors. You can edit components to display your own information just like the one you're reading now.`,
-  baseUrl: 'https://v3.woonuxt.com',
+  siteName: 'Acsweet Jewelry',
+  shortDescription: 'ACSweet Jewelry is an online fine jewelry store. It provides a modern, fast, and seamless shopping experience for stylish, confident women who believe affordable luxury should be part of every day.',
+  description: `ACSweet Jewelry offers fine jewelry that blends fashion-forward design with everyday affordability. We believe confidence shouldn't cost a fortune — just a touch of elegance for the real you.`,
+  baseUrl: 'https://www.acsweetjewelry.com',
   siteImage: 'https://user-images.githubusercontent.com/5116925/218879668-f4c1f9fd-bef4-44b0-bc7f-e87d994aa3a1.png',
   storeSettings: {
     autoOpenCart: false,
